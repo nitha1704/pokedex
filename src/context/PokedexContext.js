fa83606d57a53685d3f7424db2ae238f0c641fea
Nitha <alive1704@gmail.com>
2021-05-11 19:23:22 +0700

    q fetch 300

--- src/context/PokedexContext.js
+++ src/context/PokedexContext.js
@@ -43,7 +43,7 @@ const GlobalContext = ({ children }) => {
   };
 
   const pokemonIndexNumber = [];
-  for (let i = 1; i <= 898; i++) {
+  for (let i = 1; i <= 300; i++) {
     pokemonIndexNumber.push(i);
   }
 
